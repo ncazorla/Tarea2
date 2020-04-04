fb9c77bea6d37eed8ac2d0f040ad8e56199e7719
Niky <ncazorla.mb@gmail.com>
2020-04-04 19:44:58 -0300

    Commit B - getters, setters y constructores

--- ClaseUsuario.java
+++ ClaseUsuario.java
@@ -7,4 +7,63 @@ public class Usuario {
 	private int cantHijos;
 	private float saldo = 0;
 
+    public Usuario() {
+	}
+	
+	public Usuario(String nombre, int CI, String contrasenia, int edad, int cantHijos) {
+		this.nombre = nombre;
+		this.CI = CI;
+		this.contrasenia = contrasenia;
+		this.edad = edad;
+		this.cantHijos = cantHijos;
+		this.saldo = 0;
+	}
+ 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getCI() {
+		return CI;
+	}
+
+	public void setCI(int cI) {
+		CI = cI;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public int getCantHijos() {
+		return cantHijos;
+	}
+
+	public void setCantHijos(int cantHijos) {
+		this.cantHijos = cantHijos;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
 }
